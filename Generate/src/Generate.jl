d6db876e98637e33b1c648a902ca7a381463e4d5
sumiya11 <alexd382382@mail.ru>
2023-04-12 08:42:25 +0300

    remove generated systems from the master branch

--- Generate/src/Generate.jl
+++ Generate/src/Generate.jl
@@ -2,29 +2,51 @@ module Generate
 
 using Groebner
 
+struct Metainfo
+    nvariables
+    ground
+    reference
+end
+
+const _references = Dict(
+    "cyclic" => "Göran Björck and Ralf Fröberg, A faster way to count the solutions of inhomogeneous systems of algebraic equations, with applications to cyclic n-roots, in J. Symbolic Computation (1991) 12, pp 329-336.",
+    "katsura" => "S. Katsura, W. Fukuda, S. Inawashiro, N.M. Fujiki and R. Gebauer, Cell Biophysics, Vol 11, pages 309-319, 1987."
+)
+
 function generate_systems()
 
     systems = [
-        ("noon3", Groebner.noonn(3)),
-        ("noon4", Groebner.noonn(4)),
-        ("noon5", Groebner.noonn(5)),
-        ("cyclic4", Groebner.cyclicn(4)),
-        ("cyclic5", Groebner.cyclicn(5)),
-        ("katsura3", Groebner.katsuran(3)),
-        ("katsura4", Groebner.katsuran(4))
+        ("noon3", Groebner.noonn(3), Metainfo(3, ("finite fields", "the rationals"), "--")),
+        ("noon4", Groebner.noonn(4), Metainfo(4, ("finite fields", "the rationals"), "--")),
+        ("noon5", Groebner.noonn(5), Metainfo(5, ("finite fields", "the rationals"), "--")),
+        ("cyclic4", Groebner.cyclicn(4), Metainfo(4, ("finite fields", "the rationals"), _references["cyclic"])),
+        ("cyclic5", Groebner.cyclicn(5), Metainfo(5, ("finite fields", "the rationals"), _references["cyclic"])),
+        ("katsura3", Groebner.katsuran(3), Metainfo(3, ("finite fields", "the rationals"), _references["katsura"])),
+        ("katsura4", Groebner.katsuran(4), Metainfo(4, ("finite fields", "the rationals"), _references["katsura"]))
     ]
     
     path = (@__DIR__)*"/../../webpage/_assets/systems"
     mkpath(path)
     @info "Writing to $path"
     cd(path)
-    for (name, eqs) in systems
+    for (name, eqs, meta) in systems
         @info "Writing $name"
         dirname = "$name"
-        filename = "$name/$name.txt"
         mkpath(dirname)
+
+        filename = "$name/metainfo.txt"
+        io = open(filename, "w")
+        println(io, "$(meta.nvariables)\n$(meta.ground)\n$(meta.reference)\n")
+        close(io)
+
+        filename = "$name/$name.txt"
         io = open(filename, "w")
         println(io, "$(join(map(string, eqs), ",\n"))")
+        close(io)
+
+        filename = "$name/$name.mpl"
+        io = open(filename, "w")
+        println(io, "{\n$(join(map(string, eqs), ",\n"))\n}:")
         close(io)
     end
     nothing
@@ -50,9 +72,22 @@ function generate_markdown()
     @info "Reading from $path"
     cd(path)
     for name in readdir(".")
-        filename = "$name/$name.txt"
         md *= "\n#### $name\n\n"
-        md *= "[$name](../assets/systems/$filename)\n"
+
+        filename = "$name/metainfo.txt"
+        io = open(filename, "r")
+        nvars, ground, reference = readline(io), readline(io), readline(io)
+        md *= "System in $nvars variables over $ground.\n\n"
+        md *= "Reference: $reference\n\n"
+        close(io)
+
+        filename = "$name/$name.txt"
+        md *= "[[txt]](../assets/systems/$filename) "
+
+        filename = "$name/$name.mpl"
+        md *= "[[maple]](../assets/systems/$filename)\n"
+        
+        md *= "\n"
     end
     writeto = (@__DIR__)*"/../../webpage/systems.md"
     @info "Populating $writeto"
--- webpage/systems.md
+++ webpage/systems.md
@@ -14,29 +14,64 @@ The list of all available systems.
 
 #### cyclic4
 
-[cyclic4](../assets/systems/cyclic4/cyclic4.txt)
+System in 4 variables over ("finite fields", "the rationals").
+
+Reference: Göran Björck and Ralf Fröberg, A faster way to count the solutions of inhomogeneous systems of algebraic equations, with applications to cyclic n-roots, in J. Symbolic Computation (1991) 12, pp 329-336.
+
+[[txt]](../assets/systems/cyclic4/cyclic4.txt) [[maple]](../assets/systems/cyclic4/cyclic4.mpl)
+
 
 #### cyclic5
 
-[cyclic5](../assets/systems/cyclic5/cyclic5.txt)
+System in 5 variables over ("finite fields", "the rationals").
+
+Reference: Göran Björck and Ralf Fröberg, A faster way to count the solutions of inhomogeneous systems of algebraic equations, with applications to cyclic n-roots, in J. Symbolic Computation (1991) 12, pp 329-336.
+
+[[txt]](../assets/systems/cyclic5/cyclic5.txt) [[maple]](../assets/systems/cyclic5/cyclic5.mpl)
+
 
 #### katsura3
 
-[katsura3](../assets/systems/katsura3/katsura3.txt)
+System in 3 variables over ("finite fields", "the rationals").
+
+Reference: S. Katsura, W. Fukuda, S. Inawashiro, N.M. Fujiki and R. Gebauer, Cell Biophysics, Vol 11, pages 309-319, 1987.
+
+[[txt]](../assets/systems/katsura3/katsura3.txt) [[maple]](../assets/systems/katsura3/katsura3.mpl)
+
 
 #### katsura4
 
-[katsura4](../assets/systems/katsura4/katsura4.txt)
+System in 4 variables over ("finite fields", "the rationals").
+
+Reference: S. Katsura, W. Fukuda, S. Inawashiro, N.M. Fujiki and R. Gebauer, Cell Biophysics, Vol 11, pages 309-319, 1987.
+
+[[txt]](../assets/systems/katsura4/katsura4.txt) [[maple]](../assets/systems/katsura4/katsura4.mpl)
+
 
 #### noon3
 
-[noon3](../assets/systems/noon3/noon3.txt)
+System in 3 variables over ("finite fields", "the rationals").
+
+Reference: --
+
+[[txt]](../assets/systems/noon3/noon3.txt) [[maple]](../assets/systems/noon3/noon3.mpl)
+
 
 #### noon4
 
-[noon4](../assets/systems/noon4/noon4.txt)
+System in 4 variables over ("finite fields", "the rationals").
+
+Reference: --
+
+[[txt]](../assets/systems/noon4/noon4.txt) [[maple]](../assets/systems/noon4/noon4.mpl)
+
 
 #### noon5
 
-[noon5](../assets/systems/noon5/noon5.txt)
+System in 5 variables over ("finite fields", "the rationals").
+
+Reference: --
+
+[[txt]](../assets/systems/noon5/noon5.txt) [[maple]](../assets/systems/noon5/noon5.mpl)
+
 
